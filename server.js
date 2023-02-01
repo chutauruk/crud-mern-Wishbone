322191c54f4efe9890c4011758782aa6f28e4746
unknown <cyn.uruk@gmail.com>
2023-02-01 01:52:40 -0700

    completed project and starting readme

--- server.js
+++ server.js
@@ -1,32 +1,42 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 //Middleware
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(bodyParser.json({limit: "30mb", extended: true}))
+// app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+// app.use(express.urlencoded({ extended: false } ));
 app.use(cors());
 
+
 //MongoDB Config
+// mongoose.set("strictQuery", true);
+
 mongoose
-  .connect("mongodb://localhost:27017/mypostsDB")
-  .catch((err) => console.log(err));
+  .connect("mongodb+srv://chutauru:m33kv41n@chutaurucluster.nuaxuh3.mongodb.net/?retryWrites=true&w=majority")
+  .catch((err) => console.log(err)); 
 
 //DB Schema and Model
 const postSchema = mongoose.Schema({
-  title: String,
-  description: String,
+  status: String,
   name: String,
   age: String,
-  gender: String,
   breed: String,
   color: String,
   weight: String,
   intakedate: String,
-  spayed: String,
   vaccinated: String,
+  description: String,
   historylog: String,
+  status: String,
+  gender: String,
+  spayed: String,
+  selectedFile: String,
 });
 
 const Post = mongoose.model("Post", postSchema);
@@ -36,9 +46,8 @@ app.get("/", (req, res) => {
 });
 
 app.post("/create", (req, res) => {
-  Post.create({
-    title: req.body.title,
-    description: req.body.description,
+    const newPost = new Post({
+    status: req.body.status,
     name: req.body.name,
     age: req.body.age,
     gender: req.body.gender,
@@ -48,8 +57,11 @@ app.post("/create", (req, res) => {
     intakedate: req.body.intakedate,
     spayed: req.body.spayed,
     vaccinated: req.body.vaccinated,
+    description: req.body.description,
     historylog: req.body.historylog,
+    selectedFile: req.body.selectedFile,
   })
+    newPost.save()
     .then((doc) => console.log(doc))
     .catch((err) => log);
 });
@@ -65,6 +77,32 @@ app.get("/posts", (req, res) => {
       .then((doc) => console.log(doc))
       .catch((err) => console.log(err));
 });
+
+app.put("/update/:id", (req, res) => {
+  Post.findByIdAndUpdate(
+    { _id: req.params.id},
+    {
+      status: req.body.status,
+      name: req.body.name,
+      age: req.body.age,
+      gender: req.body.gender,
+      breed: req.body.breed,
+      color: req.body.color,
+      weight: req.body.weight,
+      intakedate: req.body.intakedate,
+      spayed: req.body.spayed,
+      vaccinated: req.body.vaccinated,
+      description: req.body.description,
+      historylog: req.body.historylog,
+      selectedFile: req.body.selectedFile,
+    }
+  )
+    .then((doc) => console.log(doc))
+    .catch((err) => console.log(err));
+});
+
+
+
 
 app.listen(5000, function () {
   console.log("Server is running");
